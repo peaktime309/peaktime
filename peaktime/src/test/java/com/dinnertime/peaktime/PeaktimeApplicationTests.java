c7cc883ac1212823ddcbef41716718dc594bc5f7
이석범 <dltjrqja01@naver.com>
2024-10-23 12:27:44 +0900

    [SETTING][BE][석범] 프로젝트 초기 세팅
    Former-commit-id: 6aacb63d417c1b1eae0b572d83c87ad70aa098a4

---
+++ peaktime/src/test/java/com/dinnertime/peaktime/PeaktimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinnertime.peaktime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeaktimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
