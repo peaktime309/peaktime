3997710db9fd942bc3507742984cd57d7686d464
hyun-young <jhy7547@naver.com>
2024-10-28 22:55:13 +0900

    [FEAT][BE][현영] 프리셋 엔티티 유저 추가
    Former-commit-id: 5f6ec5f204ba0abcad3cdb4dd55c12096907e077

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/entity/Preset.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/entity/Preset.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cccf09463eabc0b9a3400d5b4951d2f3a9af12a59ac1ca12d40d34ebac27487a
-size 1790
+oid sha256:c68bb0db4fa54f26fd59ce2250891c9976daf76f8edf295adf08d62d8a9f8241
+size 1842
