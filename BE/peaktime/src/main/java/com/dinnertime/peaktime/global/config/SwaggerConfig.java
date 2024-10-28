953313a6eec34c32f6add1eaaab84889239fe5bd
hyun-young <jhy7547@naver.com>
2024-10-28 15:59:25 +0900

    [FEAT][BE][현영] Swagger 배포 링크 주소 추가
    Former-commit-id: eea000212b2014d50888389c844fe4b2234b6c09

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/global/config/SwaggerConfig.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/global/config/SwaggerConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:889883fb65a59a7769b527533d3b538c417bcec998478e9c16fd459b56fc2c7b
-size 1682
+oid sha256:4ea3005a887dd84758326b79edc659da26beeb37330e1c5849830da86537d1ec
+size 1678
