4e91cc0c6fb4873f2ebf90bfc04bfc31fd56f2c3
hyun-young <jhy7547@naver.com>
2024-10-28 22:55:59 +0900

    [FEAT][BE][현영] 유저 조회 임시 작성 추후 수정 예정
    Former-commit-id: 3ed957b8a2f63eadfbd82e4ae22e077fe20ae113

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/repository/PresetRepository.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/repository/PresetRepository.java
@@ -1,11 +1,15 @@
 package com.dinnertime.peaktime.domain.preset.repository;
 
 import com.dinnertime.peaktime.domain.preset.entity.Preset;
+import com.dinnertime.peaktime.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PresetRepository extends JpaRepository<Preset, Long> {
 
+    List<Preset> findAllByUser(User user);
 
 }
