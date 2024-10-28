2a2073ecbce1017067148e69d826a02d71157c9f
hyun-young <jhy7547@naver.com>
2024-10-28 22:56:29 +0900

    [FEAT][BE][현영] preset 정보 저장 service 부분 작성
    Former-commit-id: f5b4d2fea743de95737d423cf11c61eb01e826ac

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/service/PresetService.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/service/PresetService.java
@@ -1,6 +1,3 @@
-package com.dinnertime.peaktime.domain.preset.service;
-
-public class PresetService {
-
-    // preset CRUD 처리
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:42417864efa31c5209c62087bc7a93afb15a2eaaf85d3eea10d26b1829a2fd7d
+size 2280
