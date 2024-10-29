b2e9d31d6327c0baa6ba16e6227c7e5809b3a729
hyun-young <jhy7547@naver.com>
2024-10-29 14:58:31 +0900

    [FEAT][BE][현영] responseDto 넣어서 데이터 응답 추가
    Former-commit-id: ae7a0ea437f849850ae56876d4accf8f1d4bc7fd

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/controller/PresetController.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/controller/PresetController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3214f151d5548b7652991f8515b8e06963505d7e9dcba20f86ea6bc2492cae79
-size 3477
+oid sha256:c15a714f8e4e60db1cc8e757f013b0bfd715fdff70c6bc8d3998e67f2ac1af5e
+size 3621
