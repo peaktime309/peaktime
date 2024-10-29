df14f9ac81d9ddd3c4ce28ec38232ec71e5ba07c
hyun-young <jhy7547@naver.com>
2024-10-29 09:20:25 +0900

    [FEAT][BE][현영] preset size 2~8자 request에 size annotation 추가
    Former-commit-id: 33c1d4bff0c5f940222d3ad6230072491bf1d004

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/service/PresetService.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/service/PresetService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42417864efa31c5209c62087bc7a93afb15a2eaaf85d3eea10d26b1829a2fd7d
-size 2280
+oid sha256:5592d326c0f1badc3ff6e73f1455232f5ff24fbd63fb9b65ec6ef7c3b2eb936f
+size 1938
