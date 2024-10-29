4585696a8b1ce333839fe2ffab46e66b265b0a5e
hyun-young <jhy7547@naver.com>
2024-10-29 22:04:24 +0900

    [FEAT][BE][현영] preset update, delete 서비스 부분 구현
    Former-commit-id: ce7149658957132aa22fc5a2bdcfde2f0334fe56

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/service/PresetService.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/service/PresetService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bae008f6f6a62cb5d28e6e6f3a57be1a02d3e58a2ccde3f71d7441e1b791c8c2
-size 2124
+oid sha256:1d643ac2865339ea078d3b5bd01a088da9c6bb8707aca9e1f838b1a8aca3b0d4
+size 3272
