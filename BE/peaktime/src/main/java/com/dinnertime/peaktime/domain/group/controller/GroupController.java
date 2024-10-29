51a30fc156ae489bb52151268d8d6691be524d12
1139091SSAFY <iiky2124@gmail.com>
2024-10-29 17:56:10 +0900

    [FIX][BE][동찬] 전체 조회 성공 메시지 수정
    Former-commit-id: c91a69ae9471e2deea46531af88ac690824c544a

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/group/controller/GroupController.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/group/controller/GroupController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64c0726279cd1a8c7ac24da5ab52c92f8112db3ecd63bb85155d52630b059849
-size 3711
+oid sha256:6c07fe06e400f120bae9ec622c59e7b93ce91299dc669a715ee1c09fbe0e300a
+size 3708
