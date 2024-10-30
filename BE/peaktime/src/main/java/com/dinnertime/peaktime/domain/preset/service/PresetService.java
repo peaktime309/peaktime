6def927d5411c4fba1f280afec6024248f15e233
hyun-young <jhy7547@naver.com>
2024-10-30 09:53:48 +0900

    [FEAT][BE][현영] 업데이트 service 부분 수정, 삭제 데이터 무결성 참조 에러처리
    Former-commit-id: c8b3229c81135d6ac0c26f3261e446633f3519be

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/service/PresetService.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/service/PresetService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d643ac2865339ea078d3b5bd01a088da9c6bb8707aca9e1f838b1a8aca3b0d4
-size 3272
+oid sha256:e72056a0b14c9418bfc48cdd0c22a8e0304a01d63a45b7f0e28f4cf13203dc47
+size 3710
