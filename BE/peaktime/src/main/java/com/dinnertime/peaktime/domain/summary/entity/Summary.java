65f7a030cc35b4659872923b8d1f8f16819e1953
hyun-young <jhy7547@naver.com>
2024-10-31 16:19:40 +0900

    [FEAT][BE][현영] private 변경
    Former-commit-id: 556c0da70630796747fc5f27f5162cd693f917e7

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/summary/entity/Summary.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/summary/entity/Summary.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d454dd261f9fef12975478746769335a792ef4ecc4dccaa1c069f987aa3a4ee1
-size 873
+oid sha256:76691fb101bb303b4753bc5cf78d3a1c58ef974139d12c5d2311284a6cd4db2b
+size 874
