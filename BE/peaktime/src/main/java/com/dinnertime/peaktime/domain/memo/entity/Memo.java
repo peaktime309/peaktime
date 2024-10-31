afa84011c00baf54c6c79543aa6b19a0ec39bf42
hyun-young <jhy7547@naver.com>
2024-10-31 15:42:00 +0900

    [FEAT][BE][현영] memo isSummaries 컬럼 삭제
    Former-commit-id: c657fde7002e82aa1c88e402e4117305eb91e927

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/memo/entity/Memo.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/memo/entity/Memo.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:557c619f496620be7258616adca059217454c8f1df1c7eb57ad6a48f139bfb57
-size 1204
+oid sha256:cb215be7cdf147fc22934e8360ffc147f7895a7ba7705a471895511805b4f3f9
+size 1033
