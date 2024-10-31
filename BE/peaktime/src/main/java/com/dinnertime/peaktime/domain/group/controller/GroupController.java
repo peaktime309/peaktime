dd7a99bd6f2afa068854d1f9378e4b1e878e471d
1139091SSAFY <iiky2124@gmail.com>
2024-10-31 11:02:14 +0900

    [FIX][BE][동찬] 그룹 생성 API 코멘트 반영
    Former-commit-id: 04dd0c82fa86aab8aacc96c84610d548498b0ed0

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/group/controller/GroupController.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/group/controller/GroupController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22850ac64837aa23ca032afb1d7fb89eee6bf317855e1204e2096a6d98768514
-size 5066
+oid sha256:a4ba76ed2c4f339d8e962a2ba9ff0b3d63a3cc9d2c86709278c37a8fed6266e5
+size 4931
