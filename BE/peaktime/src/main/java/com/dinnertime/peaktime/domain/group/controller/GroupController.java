1ba1dffae04f6be099de7bba05a232ee63e5c213
1139091SSAFY <iiky2124@gmail.com>
2024-10-31 13:25:46 +0900

    [FIX][BE][동찬] ApiResponse 추가
    Former-commit-id: 486b35b90142d8625e283aa77276d8c667c5894b

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/group/controller/GroupController.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/group/controller/GroupController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4de21f44f04ffcdbfc81a2e0a6bddd332bd698328b1388767b9d60a8dc825bcb
-size 5356
+oid sha256:a80e427ab594a63cebf9b0e3abb91010099ef9d4c0920ab9e3d45c639dd707a2
+size 6743
