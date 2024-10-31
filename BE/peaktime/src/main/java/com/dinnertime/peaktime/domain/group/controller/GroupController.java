1d472538acac00ef8ba8b4cd3290a9a74824a7a9
1139091SSAFY <iiky2124@gmail.com>
2024-10-31 11:21:46 +0900

    [FIX][BE][동찬] 함수명 변경
    Former-commit-id: 7e0f78ba02f1a81b41c3628e4db3f1fedf78343b

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/group/controller/GroupController.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/group/controller/GroupController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4ba76ed2c4f339d8e962a2ba9ff0b3d63a3cc9d2c86709278c37a8fed6266e5
-size 4931
+oid sha256:17a9d0c305f230d72b84eed971754364caaade3bfd186f1998c567a1e757c4d0
+size 4927
--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/group/service/GroupService.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/group/service/GroupService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a94abc2c6e7e077828eedbfc4bf63cbee7e0cf2f37b5c30baccecdb1df0774de
-size 5022
+oid sha256:61bbfef21b760147bd08a127ee6c18c306df62a82394d67074780a86446c8001
+size 5020
