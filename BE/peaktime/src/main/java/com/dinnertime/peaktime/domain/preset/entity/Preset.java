5c711d3476ce67d49f7e716fd1e32dc0e29e5167
hyun-young <jhy7547@naver.com>
2024-11-01 17:31:34 +0900

    [FEAT][BE][현영] entity tostring 제거
    Former-commit-id: 01e1db5aa672567d676ea3bff96f5b447235f1de

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/entity/Preset.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/entity/Preset.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1bcf536d107a2d48384b633254220d2adcc9e3dde2bfd0d258fd95b84f8676c8
-size 2221
+oid sha256:07f6a65e2783eed796c3bf0741afd1a5674637f6c11fb9d4602f2312cfe7657b
+size 2211
