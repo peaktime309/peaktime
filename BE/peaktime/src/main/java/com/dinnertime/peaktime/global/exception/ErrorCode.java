9c987df8b67a889b1e1ddbea8a3b6c1b8f1df348
이석범 <dltjrqja01@naver.com>
2024-11-01 14:51:09 +0900

    [DELETE][BE][석범] 에러코드 추가
    Former-commit-id: 6076f6adf4c7af93c1b990a022fc51c3616ad21c

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/global/exception/ErrorCode.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/global/exception/ErrorCode.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc83cf649b3303038ef921803e78bc538ac3745f69ccecaf6560d49f1842ea3c
-size 1295
+oid sha256:a82be9016042ea4d3a23d5e947818db5400b9e993079da32be79a504428461d2
+size 1381
