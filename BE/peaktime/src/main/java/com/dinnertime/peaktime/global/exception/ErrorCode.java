ec6f4adb4b72024c8a19baa6a13aa81dce345f54
이석범 <dltjrqja01@naver.com>
2024-11-01 15:00:13 +0900

    [FEAT][BE][석범] 에러코드 추가
    Former-commit-id: 0a3292497f5f155d7b590065a4c85ef1d9342edb

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/global/exception/ErrorCode.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/global/exception/ErrorCode.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a82be9016042ea4d3a23d5e947818db5400b9e993079da32be79a504428461d2
-size 1381
+oid sha256:b793befd5b539deabf22760c2fdabeb41d6a2c4abc4887d6d96f930403b11501
+size 1507
