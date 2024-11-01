c59ff319b51543d24ffe308fb4b18da2ba659a3f
이석범 <dltjrqja01@naver.com>
2024-11-01 16:21:59 +0900

    [DOCS][BE][석범] 스웨거 에러 코드 추가
    Former-commit-id: f3574ca30b383dfe2a41d0cd82f6aaf7f206629c

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/hiking/controller/HikingController.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/hiking/controller/HikingController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c14fdd48bd267e1de952c63a71cb3645a5ea8a90000b6e1939a3197fef1dcf4f
-size 3920
+oid sha256:268c449bd8883acac0b5392205b6d21c62e564c01c159e3785ece37624cb2b4a
+size 4151
