472978a875d9128ecc6f69f9d1a58ab4556372b9
hyun-young <jhy7547@naver.com>
2024-11-02 21:42:45 +0900

    [FIX][BE][현영] 미구현 service 부분 주석처리
    Former-commit-id: 78eab053950b0c0e6570bfb60e5a7aa4e57c99a3

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/summary/service/SummaryService.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/summary/service/SummaryService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad3e4e2f6ddd4c39d2e47988b86eba837d8e0f711e45156e73166dbb2b93d22b
-size 1732
+oid sha256:e14ffd5ebdfbc52233a03b9c8125584f28e9d6047864747f5974963f344a348d
+size 1754
