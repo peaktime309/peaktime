5db8d87f7f9df1481b9b782cf5d2d5ed6986ad0e
이석범 <dltjrqja01@naver.com>
2024-11-04 00:22:41 +0900

    [FEAT][BE][석범] hikingDetail 이 null이 아닐 경우만 설정
    Former-commit-id: f7846a928fb028ec80ad00acad010d3d61e59a6d

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/hiking/repository/HikingRepositoryImpl.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/hiking/repository/HikingRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b18594157e5a3e2d6a96298691aa99ef83077127366e90f0ba2ac7c03ef12d5a
-size 6384
+oid sha256:1b208fb5c55743096c57327caec659c7d7da8419dc33e7011a92fc2735fe48d1
+size 6185
