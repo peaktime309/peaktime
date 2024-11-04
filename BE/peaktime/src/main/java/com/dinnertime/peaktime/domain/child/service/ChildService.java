8a60417fdfb49ba60a270be7cda0ce5655cad0ad
ayoung-kim <ayoung7777@gmail.com>
2024-11-04 20:08:52 +0900

    [FEAT][BE][아영] 그룹의 유저 수 쿼리 수정
    Former-commit-id: 20c1c7db1d698f82d86aba4b70f3c0c6f91fc610

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/child/service/ChildService.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/child/service/ChildService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6196afcb47c328c666e8e19672a6836e10a0fd1c48eb3edb1a5ae08860ec6fe
-size 6355
+oid sha256:269cc9c868540f217af104ab78bfa383caced65eb7cf71c20c48d8f1a75fef04
+size 6317
--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/usergroup/repository/UserGroupRepository.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/usergroup/repository/UserGroupRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee9fc30ac0dc090e78da0c023b4b6c1515f6c8aa37c840ca85c6dad536dd4d25
-size 846
+oid sha256:53bb9f75eb7df99c9a3c9862a03fe3a61da9a1421b5e4acad7d178a5c8fc954b
+size 728
