3eeee5f7eddf866496cfeba5c8031c182d3ceb23
ayoung-kim <ayoung7777@gmail.com>
2024-11-04 13:28:03 +0900

    [FEAT][BE][아영] 자식 계정 조회 쿼리 수정
    Former-commit-id: ca6b4b907c37df62da258986fe1df692700b6d03

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/child/service/ChildService.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/child/service/ChildService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41f102c56cf3f0d28d63d56317c39466288ffeca9536dcd6ba2201453b265f62
-size 2442
+oid sha256:9f3f8f00607a0279bbb577354a0ea13c38f35495d191bb31b0280564afe89e9a
+size 2457
--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/user/repository/UserRepository.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/user/repository/UserRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
     Optional<User> findByUserIdAndIsDeleteFalse(long userId);
+    Optional<User> findByUserIdAndIsDeleteFalseAndIsRootFalse(long userId);
 }
