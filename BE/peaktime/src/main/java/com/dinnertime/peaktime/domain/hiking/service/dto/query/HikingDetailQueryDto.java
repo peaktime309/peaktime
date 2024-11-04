c69bb01a2726a486a8ed9094930670752cee0b62
이석범 <dltjrqja01@naver.com>
2024-11-04 12:12:31 +0900

    [CHORE][BE][석범] 주석 삭제
    Former-commit-id: 4eefeff272647c15655bd1932265ca5d03c7d533

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/hiking/service/dto/query/HikingDetailQueryDto.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/hiking/service/dto/query/HikingDetailQueryDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d4bdd643cf180eadef6baac9cb31552d35444e75903166b38f4c8f4b5fbcbbc
-size 1299
+oid sha256:5587b3192742139ec19ff5e5a8f7abdece5b8ddebaa1f4da827380e388be4363
+size 858
