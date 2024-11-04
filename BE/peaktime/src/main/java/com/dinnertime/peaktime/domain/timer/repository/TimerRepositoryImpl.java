05ab17df15982f0afcb9686851dce1a529143ddc
1139091SSAFY <iiky2124@gmail.com>
2024-11-04 10:21:54 +0900

    [FIX][BE][동찬] repeatDay 로직 수정
    Former-commit-id: 095b8c1d8d1456322575b3cfbc915bad3bd88531

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/timer/repository/TimerRepositoryImpl.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/timer/repository/TimerRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:693b7fc94650573c4c35476fdc6eda82b339753dd525abad0211861e0dbc1fd4
-size 1708
+oid sha256:8a517184e37ba9381b766aeef651145c26a0c0de2090716fae6835c1ccbe4b4c
+size 1737
