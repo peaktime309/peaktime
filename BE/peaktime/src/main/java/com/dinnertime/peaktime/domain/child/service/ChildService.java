f0aa3c76d57bbd020612822408138916036f6836
ayoung-kim <ayoung7777@gmail.com>
2024-11-04 20:05:27 +0900

    [FEAT][BE][아영] 자식 유저그룹 조회 메소드명 수정
    Former-commit-id: 608f1cddc9a5bb5e117a1fd215e272b6d29d6787

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/child/service/ChildService.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/child/service/ChildService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:262d380163c9f7d8fbdbac29cd2548bb2d8f2ab43cbe82df18a1dd83d934f25b
-size 6343
+oid sha256:c6196afcb47c328c666e8e19672a6836e10a0fd1c48eb3edb1a5ae08860ec6fe
+size 6355
