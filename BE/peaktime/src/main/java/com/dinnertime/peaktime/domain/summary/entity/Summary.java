b341a22402066ee92a0105c0d6669dba8fa1c7f0
hyun-young <jhy7547@naver.com>
2024-11-05 12:34:44 +0900

    [FEAT][BE][현영] 요약 수정 시 메모 처리 X
    Former-commit-id: dded9dd6ff62e5b525ca3217c328ad932a1602e2

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/summary/entity/Summary.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/summary/entity/Summary.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:425406c771b48cb46af6270da5a4fd0699f5e1c16cd1f5aaa24f924ad7e7eff1
-size 1582
+oid sha256:3e2116c75228647caad21be046e22e61233fa71c768b458edfcb4c7746e00d9a
+size 1456
