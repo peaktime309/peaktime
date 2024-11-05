ba71206a3645967c4db9e8e47f9cb9dbb00829f8
이석범 <dltjrqja01@naver.com>
2024-11-05 23:08:06 +0900

    [FEAT][BE][석범] 하이킹 시작시 중복 체크 및 타이머 삭제시 레디스 데이터 삭제
    Former-commit-id: d96040b56141c27149c9e55b1b58978b7e2b636a

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/timer/service/TimerService.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/timer/service/TimerService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0595ee0e1813d6be5e5dc6cb0ba31b912f55830a5fc91f1c07dbd583af7398de
-size 3363
+oid sha256:620db0d6e0d9853528768b9d553cc7bdc531ccee8f34a615b0fc2a614d60f4de
+size 3917
--- BE/peaktime/src/main/java/com/dinnertime/peaktime/global/util/RedisService.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/global/util/RedisService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d15fa065a138d988ce5e1f6e2631be5d0f305acd2df8ee3a0f10b6a3e4a7171
-size 1997
+oid sha256:05406b177c7943ebb202b815fca7970731ec482e705dbadc2efdf0fa6f68b220
+size 2240
