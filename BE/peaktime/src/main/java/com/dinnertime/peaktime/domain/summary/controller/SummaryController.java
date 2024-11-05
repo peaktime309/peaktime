21b8ea473d320d97b347ae40556c05e4620b6cc7
hyun-young <jhy7547@naver.com>
2024-11-05 12:07:10 +0900

    [FEAT][BE][현영] 요약 controller 생성
    Former-commit-id: 473df8b5ffe51526db97199d7d92535dac4f362c

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/summary/controller/SummaryController.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/summary/controller/SummaryController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d965ad5482cc0b5f73c952ceba7134388fb55bf49dd8a78a2788d04dd05e3d7
-size 603
+oid sha256:9161938d6ebfdcc45d91883a0150ada45b7277b08e8ff6f26c8d7adc380ea32e
+size 3593
