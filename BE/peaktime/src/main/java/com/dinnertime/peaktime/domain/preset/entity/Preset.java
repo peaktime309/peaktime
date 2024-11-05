5c077c753a29b68db472c1299743c6e0849b822a
hyun-young <jhy7547@naver.com>
2024-11-05 17:14:17 +0900

    [FEAT][BE][현영] addPreset this로 작성
    Former-commit-id: 19eb54b19199d775153d0017cdf2a3c92ca4afd2

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/entity/Preset.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/entity/Preset.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:abb203b146c843b6c4412f128875db2099deddb29edb2797e943dbcac61c6616
-size 3032
+oid sha256:d3a7bfdaabd1115ea57215fd41d153bb70306d3ca24ba17b3e3ed9afc9b287e5
+size 2898
--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/service/PresetService.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/service/PresetService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3dd46b0e554c01698135084bc65c47d795149352fec9d328cca3f32782c8660f
-size 4323
+oid sha256:2e297fef74d201b29c028007f4c2fbd87ad0542bc55ace39b1b3a68126cb3195
+size 4315
