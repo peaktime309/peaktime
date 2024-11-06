bd118af1933d0c666a0bfdee391eb9239f40ff22
hyun-young <jhy7547@naver.com>
2024-11-06 15:10:54 +0900

    [FEAT][BE][현영] redisService로 redisTemplate 이동
    Former-commit-id: 1424363289a2a0e36de6b504459aa521f7aa35d3

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/memo/service/MemoService.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/memo/service/MemoService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1784b5f01655b5a5957993203ec9712061c7bd3e185f796bc13d9d2c83ebcff3
-size 4278
+oid sha256:154414ec2482be21e8f23f5f9c87436f2beeb278b42a982a3e3c1648b43b64c3
+size 4223
---
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/global/util/RedisService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c80908066195501b16064aca1b8cc1fb0317143fee882cbc4bcecb17aad3e42e
+size 1189
