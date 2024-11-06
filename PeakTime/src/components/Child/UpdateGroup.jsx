aa14ae8c5cd55fd5e3c287296092e8909c6a7d63
1139091SSAFY <iiky2124@gmail.com>
2024-11-06 16:38:00 +0900

    [FIX][FE] response 수정
    Former-commit-id: 8b7fb4d50a9a6144ae44bfe897cf80f3efb50f01

--- PeakTime/src/components/Child/UpdateGroup.jsx
+++ PeakTime/src/components/Child/UpdateGroup.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3396e0d33d111294148dbcb44b1c148b1eaa238746faa4c61ed5dda15a6fe1bd
-size 12992
+oid sha256:cfd9d072c9e53b9b251dd2cdafa8d41750c968c4161acc72771c34f5f937115d
+size 12581
