aa20792ed40200b3fa9913fa27a8b2b2f616c70f
이석범 <dltjrqja01@naver.com>
2024-11-06 17:22:12 +0900

    [FIX][BE][석범] stringRedis로 변경
    Former-commit-id: 9ec4fc0359961dc8eb7993f32990f52f60d35fb7

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/global/util/RedisService.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/global/util/RedisService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d34a2dacf1b65f1730a8a14a01b5b3e48268f26aca9e97a4cdbaabc83bf7bc5f
+oid sha256:428957f1bae84f79d2dcc45eb79294600d0b5e9756eb798ae9c007e4bdd455ae
 size 5372
