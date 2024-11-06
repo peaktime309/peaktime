c7e562602141105fc6b6c3e41e8d1baab6365b19
hyun-young <jhy7547@naver.com>
2024-11-06 13:45:50 +0900

    [FEAT][BE][현영] memoList 전체 조회 시 하루 gpt 사용 횟수 함께 보내주기
    Former-commit-id: a6aa2a7d83fbc85ac3ba5064959457516298b1f0

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/memo/service/MemoService.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/memo/service/MemoService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c0de5fabce09eb96fe73bc49cb9a531f12c2f665366555473eca398623c6231
-size 3924
+oid sha256:1784b5f01655b5a5957993203ec9712061c7bd3e185f796bc13d9d2c83ebcff3
+size 4278
