f5a65fe80549be5337923b75f537cf0b14351f95
1139091SSAFY <iiky2124@gmail.com>
2024-11-07 10:08:03 +0900

    [FIX][BE][동찬] GroupRepository 쿼리 메서드명 수정
    Former-commit-id: b6d96344127948b33f704c1f82f34b5f53db5d2f

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/group/repository/GroupRepository.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/group/repository/GroupRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02362b5d6991d91af9674a9d14ecf03b1f1966085dd45a3f39c0ac32af86b1b4
-size 990
+oid sha256:b84212374511ccf667265587e8f87417e913ecb7e6385d3f5f776f94fc420022
+size 951
--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/group/service/GroupService.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/group/service/GroupService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c57c925ec9944e25e1948b804ff678ed2ecc76d7930d38dea9167847f51ff2b6
-size 6902
+oid sha256:39e38dc7d161c8cb8843bfcd5659045dbd200ec31dc4d7e21a6705bcfe62eb4e
+size 6896
