c9940afc863d6100ce2969505bac06abeb4efacb
이석범 <dltjrqja01@naver.com>
2024-11-07 09:26:47 +0900

    [FIX][BE][석범] 테스트로 제외한 url변경
    Former-commit-id: dc62d4bc76ca3a5c6252f94bb1de3bca424deb57

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/global/config/SecurityConfig.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/global/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6bf871cfa94585b3c5557777a441095e16cb0bd38d8a14aae935d06bec0a7503
-size 3692
+oid sha256:b71fa8764356a41048057c73a2a574c5d904f58a382e8cc4e4426220a580b79d
+size 3661
