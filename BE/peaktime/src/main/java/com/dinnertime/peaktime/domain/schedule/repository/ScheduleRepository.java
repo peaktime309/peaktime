e888ae0ca2545ffd73de0ac2fa4432ce095be6a7
이석범 <dltjrqja01@naver.com>
2024-11-08 00:04:57 +0900

    [FEAT][BE][석범] jpa에 맞게 메서드명 변경
    Former-commit-id: b6e0db981810c475c62aaf62d5eff56d0250a2ad

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/schedule/repository/ScheduleRepository.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/schedule/repository/ScheduleRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fcf656f537908d3ff4802e9e82b3d0d95353c770f3e5892918ae4393b7436126
-size 559
+oid sha256:79b79042c5c8a23c8c222c1e8a4142c466796d3069c7fcb22f2e61372b6cca0e
+size 557
