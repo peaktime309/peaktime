65e9d07300c2cfbe3beb4f058f4e81dc69e273c0
이석범 <dltjrqja01@naver.com>
2024-11-07 13:22:33 +0900

    [FEAT][BE][석범] 타이머 설정 추가
    Former-commit-id: ac06d01bc2a27741bc5b28429f476c06f117db42

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/global/config/SecurityConfig.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/global/config/SecurityConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ace6f2cbddafb2c9c038bccf4746c81fafd3f5d060713bf69361b8570a08ecc0
-size 3771
+oid sha256:19c18418c3b5947a3855f7d5d69c2d7ba0f05ed018e452b7a231026e4ef9dcd1
+size 3772
