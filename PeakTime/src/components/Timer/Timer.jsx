5452936da1982bcfe406253be58fc535f9047763
ayoung-kim <ayoung7777@gmail.com>
2024-11-07 17:43:26 +0900

    [FEAT][EL][아영] 대기 시간 수정
    Former-commit-id: 3b3dfb2e3ba59543106a97eb55d455b239f9b76c

--- PeakTime/src/components/Timer/Timer.jsx
+++ PeakTime/src/components/Timer/Timer.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f009b98eb3658eba1af00b01083eaa4b2a64413e877a22a8c723c2d52c148f3
-size 12945
+oid sha256:8d7a431b69a9f5217b13fcd03b3a4618574a1fc3d12d9f1ec0be064bca2dc8f0
+size 12944
