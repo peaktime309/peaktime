7495a8e9a18e3cd12d67b6eabb10c0b2c6fa8f67
1139091SSAFY <iiky2124@gmail.com>
2024-11-08 13:42:46 +0900

    [FIX][EL][동찬] UpdateGroup 수정
    Former-commit-id: 97e56b8d0387b809413172ceee67f9dee68a6f8b

--- PeakTime/src/components/Child/UpdateGroup.jsx
+++ PeakTime/src/components/Child/UpdateGroup.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61f970fd8867926a8bb54433bcecf20396c3b90e9af6404a693b1b6fc4ca6436
-size 12414
+oid sha256:49a73daadb5b20cac04ba5e10e08eb1f9dbfbc5241676c93a6c37593331e9359
+size 12328
