6d7d0acf424e636eb5a4c3381acbd06c1dccdf4d
1139091SSAFY <iiky2124@gmail.com>
2024-11-08 13:52:53 +0900

    [FIX][EL][동찬] proptypes 추가
    Former-commit-id: 305db4d09ef2b0009ecc765e962b386ef5e0d316

--- PeakTime/src/components/Child/AddGroup.jsx
+++ PeakTime/src/components/Child/AddGroup.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c0a9198ec9e989a7b4d2e55753eabf0f7a7c005bc43139810d8594ca5131617
-size 4911
+oid sha256:87a4b7163b1d4ed842f221dd9825ea1e232543948736b8d2ee1cdbfb87f6d100
+size 4959
