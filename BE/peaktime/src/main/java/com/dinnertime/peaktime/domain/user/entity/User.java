8f0bec1d8e151aaea6e441655ab60aaa8d01d607
THIP-Y <nine14141732@gmail.com>
2024-11-08 12:11:19 +0900

    [FIX][BE][승찬] Apply Code Review
    Former-commit-id: ea1de117782fc48c75d2f07a1f31f0c327d2c62a

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/user/entity/User.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87a6a1af839289c969dec2c8665c174fb39244a9a551beb445ce76fa89cacc47
-size 2429
+oid sha256:1a8139df2943a91a62b41e790450cf2210a7b360f4dd13f18b2c2b017fd2b90e
+size 2418
--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/user/service/UserService.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/user/service/UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25618742d6f75b56236281b913180e4c59aa6f6960a81aedf0d40f43be75986e
-size 4920
+oid sha256:af7a1449c13f664e8c283b21121ff891968fa0388fd6fe1a4d3c3b170fc6b724
+size 4915
