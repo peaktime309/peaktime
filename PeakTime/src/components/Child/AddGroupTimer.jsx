dea3e4eb173a59e66324c691680f4cc634315f7a
1139091SSAFY <iiky2124@gmail.com>
2024-11-08 14:29:56 +0900

    [FIX][EL][동찬] addgrouptimer startTime 포맷 수정
    Former-commit-id: b9909863669372f02df9d5c0cf721e2c95403627

--- PeakTime/src/components/Child/AddGroupTimer.jsx
+++ PeakTime/src/components/Child/AddGroupTimer.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b820073aa2d869bbfcf5f1537799aa0c496c26d237b850f88624a46bc0076e6a
+oid sha256:bd193f0e95d6a643c804598461f98d598c6e395adfba4aeafe2c720c648840ea
 size 4022
