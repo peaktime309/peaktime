7ff678f004757ea33f54863447ced322efffddca
1139091SSAFY <iiky2124@gmail.com>
2024-11-08 11:00:38 +0900

    [FIX][FE][동찬] isRepeat 삭제
    Former-commit-id: 2c23301b4e062727272720050d09a0212e543c26

--- PeakTime/src/components/Child/AddGroupTimer.jsx
+++ PeakTime/src/components/Child/AddGroupTimer.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:749dab19a60d6e3fbded8111064eb3b8d85d03cd22c822e546f1b19e5e66fa91
-size 4102
+oid sha256:26255a1c74de0ced1b8022e1a70d2bedfc4f90dc894e68080d50d0fae0b48eaf
+size 4042
