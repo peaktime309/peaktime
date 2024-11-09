f9c24c7a556369c3f845366e6f09974735fc85d2
이석범 <dltjrqja01@naver.com>
2024-11-09 17:16:39 +0900

    [FEAT][BE][석범] 하이킹 안했을 때 통계 조회시 에러발생
    Former-commit-id: 2485ea96947c7bf2ffbb67808ca7218f3edf6048

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/hiking/controller/HikingController.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/hiking/controller/HikingController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7320126bf3b20b4409ded02b25b0026895d2116e0c526dcdde03206e49fbc916
-size 8873
+oid sha256:efa3b49353e7bedf91c0108ddf40e50068e369a70021eac7b43a48ffd4db1f0f
+size 8875
--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/hiking/service/dto/response/HikingStatisticResponseDto.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/hiking/service/dto/response/HikingStatisticResponseDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6ddf2f63db35fab967491dfa276abbdeb86bee830c8ce0f4ef9cdfe2935a824
-size 2158
+oid sha256:daa56f65de4a52902a532aef1904637ad490f1a4c1b1a116ded9a13fd7e452cf
+size 2706
