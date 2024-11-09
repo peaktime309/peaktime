ab293c7ebd0b80b4f8519ad464236104476fa2e6
이석범 <dltjrqja01@naver.com>
2024-11-09 17:32:49 +0900

    [FIX][BE][석범] 선호하는 시간대 없을시 -1로 출력
    Former-commit-id: 34e1ac4587caf2be2957678ed21031a2ea591398

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/hiking/service/dto/response/HikingStatisticResponseDto.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/hiking/service/dto/response/HikingStatisticResponseDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:daa56f65de4a52902a532aef1904637ad490f1a4c1b1a116ded9a13fd7e452cf
-size 2706
+oid sha256:6dfe15e5032953f4008fe3941128d03da38e9015460db57bf127cf3c5b964669
+size 2707
