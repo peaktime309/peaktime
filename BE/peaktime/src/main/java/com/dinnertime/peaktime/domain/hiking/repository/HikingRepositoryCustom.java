1fc72eacc6557ecdf26c37baa8af251327166b8a
이석범 <dltjrqja01@naver.com>
2024-11-09 18:19:53 +0900

    [FIX][BE][석범] 선호하는 시간대를 시작시간으로 변경
    Former-commit-id: f5f9b818c791180c601b8696027f1a02299abdb1

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/hiking/repository/HikingRepositoryCustom.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/hiking/repository/HikingRepositoryCustom.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf4886a67f7b88be368334e0229a3e3923687f6372a0231eadf4dda5ecb8665f
-size 878
+oid sha256:cc96de731995c01ac4d49bfbb43e6ea8f9c108f0f35963d685095936aecf5a58
+size 953
--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/hiking/service/dto/response/HikingStatisticResponseDto.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/hiking/service/dto/response/HikingStatisticResponseDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6dfe15e5032953f4008fe3941128d03da38e9015460db57bf127cf3c5b964669
-size 2707
+oid sha256:80dc5dba191f29abc2daf863da4cce39b0d28e4bb31ebbf054ea34d7449d537b
+size 2841
