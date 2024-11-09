e4b67c1f3bd597122b9f3fb75fd3dc59aa27c44c
이석범 <dltjrqja01@naver.com>
2024-11-09 14:48:17 +0900

    [FEAT][BE][석범] fetch 추가
    Former-commit-id: 7165bb41e47a49b82f0c9fd873a0afa6d51c3a33

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/schedule/entity/Schedule.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/schedule/entity/Schedule.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:776a263a15bb053b002c3a0316c33b3dbfda29d00aa0f9cf91f900e088c64a01
-size 1621
+oid sha256:a393d518bae209ad09336651dc02e7c04b6e053eeff4701e570ec6dabea5fb5f
+size 1645
