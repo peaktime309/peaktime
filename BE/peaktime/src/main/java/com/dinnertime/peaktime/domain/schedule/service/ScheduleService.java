00064fe2dfb5052dedacf924bbb5b68dd4d6a3a8
이석범 <dltjrqja01@naver.com>
2024-11-10 17:35:51 +0900

    [FIX][BE][석범] 지연로딩으로 인한 스케쥴 추가 실패 수정
    Former-commit-id: cb91f18b6b72ff8c7eb53fa7ea934dafb0db85c4

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/schedule/service/ScheduleService.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/schedule/service/ScheduleService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d81191e88274a93eecac722e394fbb114883b4c18a07e54b84d6564ea9687a6
-size 6678
+oid sha256:c367ceafc3e45eae3a4651c5732e6cb6889ed3da5391f6318a9ddbf486b03861
+size 6939
--- BE/peaktime/src/main/java/com/dinnertime/peaktime/global/util/RedisService.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/global/util/RedisService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e3a5539aefd3a2e44d93680ef98a42e9523557d8b1ed8982493cffb1b9f2b57
-size 8782
+oid sha256:606652c91d85d7a54586b306b8a532c350ee14204f28d6e3d0b1094cfb34abff
+size 8627
