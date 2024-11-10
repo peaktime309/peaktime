5d5a6366827d7b32867128f7eed7f82ee7d91019
이석범 <dltjrqja01@naver.com>
2024-11-10 15:55:08 +0900

    [REFACTOR][BE][석범] 서브 쿼리 대신 쿼리로 변경
    Former-commit-id: d34e169ad6f1d28990f459c78195c12207136349

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/user/repository/UserRepositoryImpl.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/user/repository/UserRepositoryImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c7247b0d7797b230bc6efe77b261a9a1e77264ecec048d71b326e3ba342a79b
-size 2121
+oid sha256:e4e0cc4078c386e1ee690df89e76bc0a32a2ffbdae12c41aa4b3ba7680d335ac
+size 3619
