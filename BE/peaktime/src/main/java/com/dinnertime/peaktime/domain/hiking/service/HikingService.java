905ae6c8658f26e94389b4d2a540d8d1ce329193
이석범 <dltjrqja01@naver.com>
2024-11-10 15:54:37 +0900

    [FIX][BE][석범] 자식계정 검증 추가
    Former-commit-id: b6243eb57bb1cc7a70b8f52b8267b1cf9eb67fc4

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/hiking/service/HikingService.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/hiking/service/HikingService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9ad258047673dfd2771b267dbbcf365eed33d3352f896408f19e9c96e31ced9
-size 8193
+oid sha256:3d744b299d911c554b25f01002b2c32458021e815a17ae7c2bd796569b30deac
+size 8593
