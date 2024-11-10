3984211a4ac3e5fcc657d6c519ca02a6f3caae17
unknown <iiky2124@gmail.com>
2024-11-10 22:40:36 +0900

    [FIX][EL][동찬] AddChild 페이지 수정
    Former-commit-id: 2d27146445541c392add0a48d61112defae48564

--- PeakTime/src/components/Child/AddChild.jsx
+++ PeakTime/src/components/Child/AddChild.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b6e6880cb77979aa2ab6292bbfa74af6bc98799d0bdbf973c6dc42c8e63852f
-size 4100
+oid sha256:5a7a9b6dcd2d7a5eebb86161e9c67e69ea099c533a4a8c9fcad7e78cd41ecc95
+size 5168
