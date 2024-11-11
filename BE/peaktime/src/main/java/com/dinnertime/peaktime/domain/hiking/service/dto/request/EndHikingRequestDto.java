5728dac9e8093e41649ea5feebb1ff071a4a5065
이석범 <dltjrqja01@naver.com>
2024-11-11 12:29:25 +0900

    [REFACTOR][BE][석범] 사용안한 import 삭제
    Former-commit-id: 611a83325964902e5a9a7c0e09c1c0085e31cca3

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/hiking/service/dto/request/EndHikingRequestDto.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/hiking/service/dto/request/EndHikingRequestDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c581f0ad630b64cdc49135aa2eb9f14c09c33e1d61dbccb10778803a73b58b8
-size 663
+oid sha256:a8e6a62771d68e3aa296f3d1f345e1fe43b42b35e873b55278ff6296b18003b9
+size 602
