80007b1e74d2995a5c1f2c34ce39f6359c58946a
ayoung-kim <ayoung7777@gmail.com>
2024-11-11 15:24:25 +0900

    [FEAT][EL][아영] public 경로 수정
    Former-commit-id: e93482288aeded98a0cda1f6dff567a5ad9ffec9

--- PeakTime/electron/Main.js
+++ PeakTime/electron/Main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c52f958b906fe1f36731b22100942f7f44f08be50e88f53a852661a20d4f6a21
-size 4144
+oid sha256:3936ce932342fac0dc0694cdeaaa1d548d94420b61539c61a2cfccfcabd11b02
+size 4203
