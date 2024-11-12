3581d97d539a5032435048b2bad6cd0766582551
정영일 <bubbleutopia@naver.com>
2024-11-12 11:17:52 +0900

    [DESIGN][EL][영일] 그룹 수정 css 적용
    Former-commit-id: 70625bc7aad09dcb81ac956a9a3b09e2e1927389

--- PeakTime/src/components/Child/UpdateGroup.jsx
+++ PeakTime/src/components/Child/UpdateGroup.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3d8f603dee6543a28f972309d9c71eb8c807b5dfd35cb380559d39729765d2f
-size 12842
+oid sha256:ad00f0402d01e43d326cdd30876ca4d5441854c200ce533dab43d800d2f07e6d
+size 15387
