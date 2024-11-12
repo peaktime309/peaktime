a22ca9f2d69d6f117292ad0a7c2828a26464d14f
1139091SSAFY <iiky2124@gmail.com>
2024-11-12 13:26:15 +0900

    [DESIGN][EL][동찬] 하이킹 및 캘린더
    Former-commit-id: 42abf284ef2ec866c3ac9fa040db79c8afc8e816

--- PeakTime/src/components/Report/DailyReport.jsx
+++ PeakTime/src/components/Report/DailyReport.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1db0cf656e8fbd0cd00e6075e0ad1756d670af018af198089a8e66ac4ff381f
-size 5284
+oid sha256:706ad0dade1abfcbd2237f5c49035eba8d077c966b2cd144b147ed8f8db9200d
+size 8138
