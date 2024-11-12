b84559aeea4c60607606386e055a4c6ebf5d5f32
이석범 <dltjrqja01@naver.com>
2024-11-12 12:37:49 +0900

    Revert "[FEAT][EL][석범] 렌더러 수정"
    This reverts commit b47163a7837296bc8e92592d7aee27ec0927dffe [formerly 1b46ea9d322d3f7494596d3448c514db0f940112].
    
    
    Former-commit-id: b5a726971c09a2c1d5a82e9107f0fcbe30d8cb2a

--- PeakTime/src/pages/Loft.jsx
+++ PeakTime/src/pages/Loft.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d757f15d48f17fd588e34656bb3c93038e5192847a6986554f34c03a44d3b9e
-size 7277
+oid sha256:223f04216ff7ecb2842552f7e3696b2b22aed99a99b1faec6d6593292cf11f06
+size 7303
