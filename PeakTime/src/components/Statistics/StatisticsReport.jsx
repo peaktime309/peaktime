3f749ec132a66e029535626060e2ea79712d8d13
1139091SSAFY <iiky2124@gmail.com>
2024-11-12 17:47:43 +0900

    [DESIGN][EL][동찬] 통계 페이지 차트 너비 수정
    Former-commit-id: 1626915cfe1b9584ecfc5fdf27fad5d8b36bafea

--- PeakTime/src/components/Statistics/StatisticsReport.jsx
+++ PeakTime/src/components/Statistics/StatisticsReport.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af756b1181a6988c56aab7f2a4e3dc743d4f9d0fc811c2b3e30aa589b1b83f84
+oid sha256:4bd1d3d2dcc8fcb89897df97cc68fe63487bf62369c55644504d3da002c8cd24
 size 4943
