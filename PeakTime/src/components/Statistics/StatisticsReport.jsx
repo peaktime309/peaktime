a208197d947b54c395a5c35cc1437c36d0182f00
1139091SSAFY <iiky2124@gmail.com>
2024-11-12 16:09:13 +0900

    [DESIGN][EL][동찬] 통계 페이지
    Former-commit-id: 7140bb9e46cc70c3955caf873a0719cffee3a354

--- PeakTime/src/components/Statistics/StatisticsReport.jsx
+++ PeakTime/src/components/Statistics/StatisticsReport.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f6a58743a2be520047c9a1a84a64071f61a34b85483797a05fee5125e565e80
-size 4481
+oid sha256:af756b1181a6988c56aab7f2a4e3dc743d4f9d0fc811c2b3e30aa589b1b83f84
+size 4943
