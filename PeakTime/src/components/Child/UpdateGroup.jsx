19ca60b5c9d3fb67bf1f8b3a471eef75cd931d4c
정영일 <bubbleutopia@naver.com>
2024-11-12 14:29:42 +0900

    [DESIGN][EL][영일] 예약시간 추가 모달 css 적용
    Former-commit-id: 520057044587262e3493565a9936577119313eac

--- PeakTime/src/components/Child/UpdateGroup.jsx
+++ PeakTime/src/components/Child/UpdateGroup.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad00f0402d01e43d326cdd30876ca4d5441854c200ce533dab43d800d2f07e6d
-size 15387
+oid sha256:af7853d7203c7b030f6013e84b667cbf94e2c52945cb9d02d0eadd0c2a0f12c9
+size 15432
