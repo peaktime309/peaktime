8387ba3f94c71313d650a5095fcfa36c73ab9add
ayoung-kim <ayoung7777@gmail.com>
2024-11-13 16:13:20 +0900

    [FEAT][EL][아영] 프로그램 차단 시 alert창
    Former-commit-id: ef340654082f237759e99046c4af96a4ff62578c

--- PeakTime/electron/Main.js
+++ PeakTime/electron/Main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:88393111ae40c4b3e1875555725bea7eacf6c6029b50c49c4ac196b5c8c9cb3b
-size 5162
+oid sha256:ab02aef2d5da97dfc477db56ecf10ca800ff09ccfa034fd74138340d888ff67f
+size 5170
--- PeakTime/electron/processWatcher.js
+++ PeakTime/electron/processWatcher.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d5d7a423c9f19a18b04bef71ca3f89592e482891a17b24f1bdfd5c831bf725f7
-size 3092
+oid sha256:7c9e9f6822af814dce021f7761c0a856b2481755655a3faed4a50d6154356f89
+size 3414
