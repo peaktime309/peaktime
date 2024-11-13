eee144b20af4af009525d20ab9b6fe494d26c27a
hyun-young <jhy7547@naver.com>
2024-11-13 12:51:33 +0900

    [FEAT][BE][현영] 200 SCHEMA 변경
    Former-commit-id: 3ef430b00e3bd0ec35af7f91570252ba2bdae458

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/controller/PresetController.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/controller/PresetController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba83e737d16b0a76d2828cc8369861a9ecf0cd8f87ed6489994dba9f0d8c5941
-size 8416
+oid sha256:c65ca77a045b972bc1868f1a147770ed1ac07ed0d71156a11f9fd7a08520dc88
+size 8409
