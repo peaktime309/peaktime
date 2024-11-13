26fe8327485b0562d864c9d7e9653fabf5a08093
Nagi <nine14141732@gmail.com>
2024-11-13 14:26:55 +0900

    [FIX][EL][승찬] Apply Code Reivew
    Former-commit-id: 3a5ec2a6f262b592b429c141ff629dd74323b32c

--- PeakTime/src/pages/LoginPage.jsx
+++ PeakTime/src/pages/LoginPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4e3cf67f34ed384d4cfaf23eb0ba4ad3090b4fa61d9878d68160dc39023e84b
-size 4865
+oid sha256:cdc54381a7ee5b1cc90de1cab4750f87b8f167ca372a646792bf41d53f38970c
+size 4869
