69d8994b971b682b27990c5fb8ed1d67a68ba0c9
이석범 <dltjrqja01@naver.com>
2024-11-13 22:14:28 +0900

    [FEAT][EL][석범] 리프레시 토큰 쿠키에 추가
    Former-commit-id: c882be00ce38932a8acd195adee2df4768f85c43

--- PeakTime/src/pages/LoginPage.jsx
+++ PeakTime/src/pages/LoginPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cdc54381a7ee5b1cc90de1cab4750f87b8f167ca372a646792bf41d53f38970c
-size 4869
+oid sha256:e7644590e88a53873ec4dadc4d43f2141d1c86cf17f7a7497ec4c221c53ef3bf
+size 5065
