2cd86d245e13868234a6d4803c1c938a6a0a19ab
Nagi <nine14141732@gmail.com>
2024-11-13 12:53:05 +0900

    [FEAT][EL][승찬] Complete Signup
    Former-commit-id: 5535ccd4c02bb4b3d1a94697c2d6d22fdaa304ee

--- PeakTime/src/pages/SignupPage.jsx
+++ PeakTime/src/pages/SignupPage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:025a1a9b461d0650355ac2efc87cc6874abda648b23150dd0d8ab8560223088b
-size 10569
+oid sha256:f9aa13d0d380c71119b9be384acb3c74662e227c5dc3bb456b505215bc18f60e
+size 21967
