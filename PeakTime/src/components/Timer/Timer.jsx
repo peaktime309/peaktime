b0a75d9b9204cd30fc086bb916f2a09680f40f98
ayoung-kim <ayoung7777@gmail.com>
2024-11-14 13:32:35 +0900

    [FIX][EL][아영] 메인 계정 권한 전달
    Former-commit-id: 32d0e8b8238832ac3a8947a6a71f99f07c5db65e

--- PeakTime/src/components/Timer/Timer.jsx
+++ PeakTime/src/components/Timer/Timer.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:111e10015babadbffc8514efc28a349becfe81ab4e80abe759f5d170e0ba56cb
-size 21371
+oid sha256:c6af1f435a14593ece75243c9220ff39d69807bf613ddb096d5962588453ec68
+size 21390
