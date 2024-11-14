fc43181e2a7eb2939f6e86095fe7cfb6744b843e
정영일 <bubbleutopia@naver.com>
2024-11-14 14:27:35 +0900

    [FEAT][EX][영일] url 추가 웹소켓 연결
    Former-commit-id: 0832502633d3f1838352ecd0d7d3bd5e35e3daeb

--- EX/peaktime/js/background.js
+++ EX/peaktime/js/background.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aaacb1485d520fceadc16e50284816bb61e8079a788777a6c4de7b8c1c0d15eb
-size 2071
+oid sha256:0c8dda750b7b1222720922dc4e21c1176d21ecb93bfb653483f90e6fc031580e
+size 2115
--- EX/peaktime/js/webSocket.js
+++ EX/peaktime/js/webSocket.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6b8ac8d089014ab7924fe771d3e370be81dacff7af663424bd098ba41a0ca50
-size 2898
+oid sha256:b8b051ff759ebfe0553c571e39bd1fe2523a111056b75c43c4e154ead6278281
+size 3037
