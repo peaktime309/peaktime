7ea5669b6c354c4bf6ee06395a24aa0cc4831c8a
이석범 <dltjrqja01@naver.com>
2024-11-14 10:39:09 +0900

    [CHANGE][BE][석범] 메모 길이 최대 15자로 수정
    Former-commit-id: 74e4756be67b3cb45848d61f06f45092d77498c9

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/memo/entity/Memo.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/memo/entity/Memo.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05a1a21c83d3f328a387a29046f03a23d6f0e0b9da6aa0de3fc79ed970da0018
-size 1883
+oid sha256:2316051f7629a9b24e7a2edbdb04d4c159b491f3c67179c2bedd3eb9fff2373c
+size 1895
