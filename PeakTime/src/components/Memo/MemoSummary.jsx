516449283bf6521ac58cfec173cd112907d00772
hyun-young <jhy7547@naver.com>
2024-11-14 12:18:46 +0900

    [FEAT][EL][현영] 요약 pdf형식도 추가변환해서 저장시키기
    Former-commit-id: c5c27c702c4c08f0e036e83756b2982e24ee3edd

--- PeakTime/src/components/Memo/MemoSummary.jsx
+++ PeakTime/src/components/Memo/MemoSummary.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4f2dda43c69ff7289a52cc8274f0e25c9425ffcc7d95e5e261dc84a8cb76799
-size 3807
+oid sha256:95b68c0094ee1585fa2f617b28d799abf8675c57a334ddf145f3b4925e6517a5
+size 4804
