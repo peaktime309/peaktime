32da6324e591cbbfb1c37b306ce14fd0a709931e
이석범 <dltjrqja01@naver.com>
2024-11-14 14:06:54 +0900

    [FEAT][BE][석범] URL 추가시 응답 메시지 추가
    Former-commit-id: 7c5f807bd8fab9089a4a67178dd86a9c8adabead

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/controller/PresetController.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/preset/controller/PresetController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c65ca77a045b972bc1868f1a147770ed1ac07ed0d71156a11f9fd7a08520dc88
-size 8409
+oid sha256:c2baa753655e1b9f9b885e8122b60110b6fb180b32915dce918da4363118058d
+size 8561
