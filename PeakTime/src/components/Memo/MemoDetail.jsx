98753ad9080e745b9a4b32e324f8ba0fc81711e9
1139091SSAFY <iiky2124@gmail.com>
2024-11-15 10:00:59 +0900

    [FIX][EL][동찬] 메모 칸 높이 수정
    Former-commit-id: 0d2865c3785d5564e8f76900f0b6b798ab4ecd8d

--- PeakTime/src/components/Memo/MemoDetail.jsx
+++ PeakTime/src/components/Memo/MemoDetail.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48a4eaaab2ddc225c2cfe5c6bbba473a58608beacf7e04bb8e6dabce42406e82
-size 15225
+oid sha256:3ed7630d4a34b01705936b7a544836bcf409bf85c563289454e517071d9818c4
+size 15290
