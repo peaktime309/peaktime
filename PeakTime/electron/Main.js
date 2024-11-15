76282cb7b83896123c2e70692a4d4e7547202bf1
ayoung-kim <ayoung7777@gmail.com>
2024-11-15 17:49:47 +0900

    [FIX][EL][아영] 일랙트론 강제 종료 시 에러 수정
    Former-commit-id: 5cff7ff02e06d1e7bb82b8804aa6601959281a4f

--- PeakTime/electron/Main.js
+++ PeakTime/electron/Main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b7b57c9e4de96ff9b07282fa1c97d3464e2f43f7f44bd563ea31fdb36086875
-size 5550
+oid sha256:bc79a541831b25bdcea04f33fc91678af186ad786d547eac34d38261cd9184a4
+size 5699
