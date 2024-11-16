03ca2acf135ef965ba67b470576508e437cda688
1139091SSAFY <iiky2124@gmail.com>
2024-11-16 21:21:54 +0900

    [FIX][EL][동찬] updategroup에서 타이머 목록 스크롤바 추가
    Former-commit-id: 271db6ed31d355dcde1024c34cf2835593da8364

--- PeakTime/src/components/Child/UpdateGroup.jsx
+++ PeakTime/src/components/Child/UpdateGroup.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b27ed17cf6f00ecd3fa395387027910b989215c47f9b1893924c3b7be991f92e
-size 11180
+oid sha256:fcfba02feff42c987b24b86d36e7e13d0e13e5c82e8a997e09b5bf6d525498b8
+size 11257
