5c522872af2fc982c595b9dc63f2ea359a01af25
unknown <iiky2124@gmail.com>
2024-11-16 14:04:51 +0900

    [FIX][EL][동찬] 그룹 추가/삭제 시 버그 수정
    Former-commit-id: bacb95b40265c24ef12ffe765bcebd7c9d54f1e6

--- PeakTime/src/components/Child/ChildList.jsx
+++ PeakTime/src/components/Child/ChildList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c935917ce03248cddfd4d2eeaa567e7166cdd09b5272c9aebe05150db703e7f
-size 2943
+oid sha256:22a4bb12a45d4c2a3990ca1e2fd1840d8fcddf7e6e81e77f0b8aef11992f6ba0
+size 3219
