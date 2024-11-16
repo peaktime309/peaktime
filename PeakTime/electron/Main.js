c9440d9120dce96ee32b39e9fd0f8967133f9688
이석범 <dltjrqja01@naver.com>
2024-11-16 17:53:26 +0900

    [FEAT][EL][석범] 윈도우 크기 변경
    Former-commit-id: eaab2cd0bf59a709b907b277afd42d5ee10e3328

--- PeakTime/electron/Main.js
+++ PeakTime/electron/Main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b55bc675ef6cb516eb8c4d9350e39dde1e40356d2e562361fefdc2ab53d15dcb
-size 7850
+oid sha256:cf5f89aa686abf97e1de83d89b802331ab9a414e3c9cfd2781da757485142985
+size 7851
