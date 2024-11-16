f6ae9455f3a7fae58e00c9368ceedffb9931f7e4
1139091SSAFY <iiky2124@gmail.com>
2024-11-16 17:39:12 +0900

    [FIX][EL][동찬] 프리셋 목록 레이아웃 수정
    Former-commit-id: 93ded146a5588deb4593de4014c825bd321a8033

--- PeakTime/src/components/Preset/PresetList.jsx
+++ PeakTime/src/components/Preset/PresetList.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce20b99e0ccff4e9cc676a8cc9d9e903c5d4e25c87ab7a8364c4565ef9e8a212
-size 5867
+oid sha256:ba58348d1677efe30c25c42a54ca0856d2570690d09e0c054508ce131ca171ed
+size 6392
