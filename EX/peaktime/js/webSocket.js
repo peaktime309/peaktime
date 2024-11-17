e1598ed9d7d912567925f451f972647a458965f9
이석범 <dltjrqja01@naver.com>
2024-11-17 14:35:05 +0900

    [FEAT][EX][석범] 브라우저 열때 소켓 연결
    Former-commit-id: fadab19d387e4e1d988384565f56fbef6988c406

--- EX/peaktime/js/webSocket.js
+++ EX/peaktime/js/webSocket.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64291259617d8e2f01b5a3b9ca805ddbe91315ec98f46641edc8bbea3b022b0e
-size 3528
+oid sha256:c801587557bb107da01665dacd71e767e3b38ab241a0f65abb329d58e662eee6
+size 3626
