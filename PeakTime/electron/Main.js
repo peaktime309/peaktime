9d908aa5e6f1fa6f97f703c3f2ffe6c3be0827e7
이석범 <dltjrqja01@naver.com>
2024-11-17 23:12:32 +0900

    [FEAT][EL][아영] 소켓 연결 수정
    Former-commit-id: 5a5014b34850f4ccec38f496fc6be72e8d83efe9

--- PeakTime/electron/Main.js
+++ PeakTime/electron/Main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86fa623e8efdf61cc78fd4a1d74d24e53810af19de12ba3180958ce7326912a2
-size 8365
+oid sha256:4d096993fccca375b4b3238fa11bd0aeb4b6f7b299ebd12d282b30eeebbd7c51
+size 8434
--- PeakTime/electron/renderer.js
+++ PeakTime/electron/renderer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a3c9784014ff4d4f5437779d6b2a4e418759a68f636040e91fb3e76944c0a18
-size 4240
+oid sha256:e8cad433fca0171c37953db66f189fe808e6ef4110ade2951c91433aa9d37287
+size 4738
