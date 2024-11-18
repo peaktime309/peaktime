297ada93351c7d74938770c8ed9c0c4e8e2cdfe5
이석범 <dltjrqja01@naver.com>
2024-11-18 09:49:12 +0900

    [FIX][BE][석범] 하이킹 디테일 에러 수정
    Former-commit-id: 3037d2d104da3d2293c4d12746f7749f76ea2ac7

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/hiking/controller/HikingController.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/hiking/controller/HikingController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:542efb52c0e36323d3e59e3091031080b8bb24f5945983d49d87f258fcdb9a44
-size 8776
+oid sha256:6b1179537e53588154a9d6de64fc7dbdd43f15622703837871f48a5e5bda7ca3
+size 8734
--- BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/hiking/service/dto/response/HikingDetailResponseDto.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/domain/hiking/service/dto/response/HikingDetailResponseDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09b1a66201da61c9ec225118998e319689ba246acb1eaf817fc4b7f3f8a60386
-size 2335
+oid sha256:8ae273e1729d8667c3bcf6cd07ff14ba1bc2286d9689c4a07e0fa80505c0f273
+size 2922
