ea1e05c631c3a2321ed517a5778431fb79a94db6
1139091SSAFY <iiky2124@gmail.com>
2024-11-18 15:33:07 +0900

    [FIX][EL][동찬] 텍스트 및 아이콘 빨강 → 주황 수정
    Former-commit-id: 04372267bf6a55a5168458bb1ea84cf72bfd3bd6

--- PeakTime/src/pages/HomePage.jsx
+++ PeakTime/src/pages/HomePage.jsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:49de2e4a3900312cb94c25846e631c9109cbf7a966038ff5c243dbca54306bbb
+oid sha256:6bacf302ef7117606282e0e9114a0b4288141d6fc13897d952c57674cf706ab9
 size 2703
