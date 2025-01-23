5e48eca678686acdbc36d3b223614a188c30b50d
이석범 <dltjrqja01@naver.com>
2025-01-23 23:57:17 +0900

    [REFACTOR][ALL][석범] url 수정

--- BE/peaktime/src/main/java/com/dinnertime/peaktime/global/config/SwaggerConfig.java
+++ BE/peaktime/src/main/java/com/dinnertime/peaktime/global/config/SwaggerConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72d08d3bd32844a69aab0e28c596209bfdeb813519fd13875b3ae4c4f899069b
-size 1751
+oid sha256:2fd0d7837a9f29b722272fd36a025d542b77afd2cc9dab7106139f40ec5b9815
+size 1659
